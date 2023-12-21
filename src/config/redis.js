ea012ca5da251c5cccbef0ea64ff24f8b70cfeb1
KevOneRedOne <kevinalves@hotmail.fr>
2023-12-21 12:49:32 +0100

    feat(redis): upadte host

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,7 +1,7 @@
 
 
 const redisOptions = {
-    host: '', // replace with your Redis host
+    host: '172.21.0.1', // replace with your Redis host
     port: 6379, // replace with your Redis port
     password: 'root', // replace with your Redis password
 };
